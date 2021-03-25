8f250385611e2908ba29a92e1a63da4bc6aa43af
Ben Muschol <ben.muschol@airbnb.com>
2021-03-25 16:31:38 -0400

    More logging

--- src/main/java/com/airbnb/billow/jobs/BaseAWSDatabaseHolderRefreshJob.java
+++ src/main/java/com/airbnb/billow/jobs/BaseAWSDatabaseHolderRefreshJob.java
@@ -2,11 +2,13 @@ package com.airbnb.billow.jobs;
 
 import com.airbnb.billow.AWSDatabaseHolder;
 import com.codahale.metrics.Counter;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
 
+@Slf4j
 public abstract class BaseAWSDatabaseHolderRefreshJob implements Job {
     public static final String DB_KEY              = "db";
     public static final String FAILURE_COUNTER_KEY = "failure_counter";
@@ -18,8 +20,10 @@ public abstract class BaseAWSDatabaseHolderRefreshJob implements Job {
         try {
             increment(START_COUNTER_KEY, context);
             ((AWSDatabaseHolder) context.getScheduler().getContext().get(DB_KEY)).rebuild();
+            log.info("[job success] {} completed", this.getClass().toString());
             increment(SUCCESS_COUNTER_KEY, context);
         } catch (SchedulerException e) {
+            log.error("[job failure] {} completed", this.getClass().toString());
             increment(FAILURE_COUNTER_KEY, context);
             throw new JobExecutionException(e);
         }
